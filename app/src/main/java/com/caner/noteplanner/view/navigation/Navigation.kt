a8c78410ee6234c41194b32085c75fcba98850f6
canergulgec <caner@armut.com>
2021-11-10 13:06:02 +0300

    Remove unused bottomSheetContainer in Navigation.kt

--- app/src/main/java/com/caner/noteplanner/view/navigation/Navigation.kt
+++ app/src/main/java/com/caner/noteplanner/view/navigation/Navigation.kt
@@ -12,44 +12,38 @@ import com.caner.noteplanner.view.notes.NotesScreen
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-import com.google.accompanist.navigation.material.ModalBottomSheetLayout
-import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
 
 @Composable
 fun SetNavigation() {
-    val bottomSheetNavigator = rememberBottomSheetNavigator()
-    val navController = rememberAnimatedNavController(bottomSheetNavigator)
+    val navController = rememberAnimatedNavController()
     val actions = remember(navController) { MainActions(navController) }
 
-    ModalBottomSheetLayout(bottomSheetNavigator) {
-        AnimatedNavHost(navController, startDestination = Screen.Dashboard.route) {
-            composable(
-                Screen.Dashboard.route
-            ) {
-                NotesScreen(actions)
-            }
+    AnimatedNavHost(navController, startDestination = Screen.Dashboard.route) {
+        composable(
+            Screen.Dashboard.route
+        ) {
+            NotesScreen(actions)
+        }
 
-            composable(
-                Screen.AddEditNote.route,
-                arguments = listOf(navArgument(Constants.NOTE_ID) {
+        composable(
+            Screen.AddEditNote.route,
+            arguments = listOf(navArgument(Constants.NOTE_ID) {
+                type = NavType.IntType
+                defaultValue = -1
+            },
+                navArgument(Constants.NOTE_COLOR) {
                     type = NavType.IntType
                     defaultValue = -1
-                },
-                    navArgument(Constants.NOTE_COLOR) {
-                        type = NavType.IntType
-                        defaultValue = -1
-                    }
-                )
-            ) {
-                val color = it.arguments?.getInt(Constants.NOTE_COLOR) ?: -1
-                AddEditNoteScreen(actions, color)
-            }
+                }
+            )
+        ) {
+            val color = it.arguments?.getInt(Constants.NOTE_COLOR) ?: -1
+            AddEditNoteScreen(actions, color)
         }
     }
 }
 
 class MainActions(navController: NavController) {
-
     val upPress: () -> Unit = {
         navController.navigateUp()
     }
