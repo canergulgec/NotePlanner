208b4ca4010fd2550838ec0dac5c1a8a1bc5e459
canergulgec <caner@armut.com>
2021-11-10 12:37:07 +0300

    Set status bar color using Accompanist systemuicontroller Add screenshots of the application

--- app/src/main/java/com/caner/noteplanner/view/MainActivity.kt
+++ app/src/main/java/com/caner/noteplanner/view/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import com.caner.noteplanner.view.navigation.SetNavigation
 import com.caner.noteplanner.ui.theme.NotePlannerTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,9 +20,23 @@ class MainActivity : ComponentActivity() {
             NotePlannerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+                    SetStatusBarColor()
                     SetNavigation()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun SetStatusBarColor() {
+    val systemUiController = rememberSystemUiController()
+    val color = MaterialTheme.colors.onSurface
+
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = color,
+            darkIcons = true
+        )
     }
 }
